1cc54e802e33d5115f8e329050397f6a2a4840b6
GareevaFarida <gareeva.farida@gmail.com>
2019-11-21 15:45:48 +0300

    comment

--- admin-ui/src/main/java/ru/geekbrains/spring2/AdminUiApplication.java
+++ admin-ui/src/main/java/ru/geekbrains/spring2/AdminUiApplication.java
@@ -8,7 +8,7 @@ public class AdminUiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AdminUiApplication.class, args);
-
+        System.out.println("hi!");
     }
 
 }
