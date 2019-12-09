1afa2943936a56f46f174e8c265004ff68c6e7db
GareevaFarida <gareeva.farida@gmail.com>
2019-12-09 14:48:17 +0300

    ПР5. Применен балансировщик нагрузки zuul.

---
+++ zuul-proxy/src/test/java/ru/geekbrains/ZuulProxyApplicationTests.java
@@ -0,0 +1,15 @@
+package ru.geekbrains;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@SpringBootTest
+class ZuulProxyApplicationTests {
+
+//    @Test
+//    void contextLoads() {
+//    }
+
+}
