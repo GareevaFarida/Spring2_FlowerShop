71b444bae309b135857ee5bb7685d54ff7a2a8ef
GareevaFarida <gareeva.farida@gmail.com>
2019-12-10 00:43:37 +0300

    ПР5. Применен балансировщик нагрузки zuul и eureka

--- zuul-proxy/src/main/java/ru/geekbrains/ZuulProxyApplication.java
+++ zuul-proxy/src/main/java/ru/geekbrains/ZuulProxyApplication.java
@@ -3,10 +3,12 @@ package ru.geekbrains;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableZuulProxy
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+@EnableEurekaClient
 @SpringBootApplication
 public class ZuulProxyApplication {
 
