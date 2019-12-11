d44060db989dc50bd989ecc6310da544866fd77d
GareevaFarida <gareeva.farida@gmail.com>
2019-12-12 00:05:21 +0300

    ПР5. Выделен отдельный сервис picture-service. Используется балансировщик нагрузки zuul и discovery-service eureka

--- picture-service/src/main/java/ru/geekbrains/PictureServiceApplication.java
+++ picture-service/src/main/java/ru/geekbrains/PictureServiceApplication.java
@@ -2,14 +2,12 @@ package ru.geekbrains;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
-//@EnableEurekaClient
 @SpringBootApplication
-public class UserUiApplication {
+public class PictureServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UserUiApplication.class, args);
+        SpringApplication.run(PictureServiceApplication.class, args);
     }
 
 }
--- picture-service/src/main/java/ru/geekbrains/conroller/PictureController.java
+++ picture-service/src/main/java/ru/geekbrains/conroller/PictureController.java
@@ -1,7 +1,8 @@
-package ru.geekbrains.controllers;
+package ru.geekbrains.conroller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,6 +20,7 @@ public class PictureController {
 
     private final PictureRepository pictureRepository;
 
+    @Autowired
     public PictureController(PictureRepository pictureRepository) {
         this.pictureRepository = pictureRepository;
     }
@@ -26,6 +28,8 @@ public class PictureController {
     @GetMapping("/picture/{pictureId}")
     public void adminDownloadProductPicture(@PathVariable("pictureId") Long pictureId,
                                             HttpServletResponse response) throws IOException {
+        System.out.println("------------------------------------------------");
+        System.out.println("GETTING PICTURE /picture/"+pictureId);
         logger.info("Picture {}", pictureId);
         Optional<Picture> picture = pictureRepository.findById(pictureId);
         if (picture.isPresent()) {
